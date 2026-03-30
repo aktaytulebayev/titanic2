a3a4a360f79de23927cadd1c6e3ce0ee4841a999
unknown <LA@DESKTOP-6N465R5.NITEC.KZ>
2026-03-30 18:02:20 +0500

    Добавлены requirements.txt и runtime.txt

--- .ipynb_checkpoints/app-checkpoint.py
+++ .ipynb_checkpoints/app-checkpoint.py
@@ -2,21 +2,31 @@ import streamlit as st
 import joblib
 import numpy as np
 
+# --------------------------
 # Загружаем модель
+# --------------------------
 model = joblib.load("model.joblib")
 
+# --------------------------
+# Заголовок приложения
+# --------------------------
 st.title("🚢 Titanic Survival Predictor")
 st.write("Введите данные пассажира:")
 
+# --------------------------
 # Ввод данных пользователем
+# --------------------------
 pclass = st.selectbox("Класс билета", [1, 2, 3])
 sex = st.selectbox("Пол", ["Мужчина", "Женщина"])
 age = st.slider("Возраст", 1, 80)
 fare = st.number_input("Стоимость билета", 0.0, 500.0)
 
+# Преобразуем пол в числа
 sex = 0 if sex == "Мужчина" else 1
 
+# --------------------------
 # Кнопка предсказания
+# --------------------------
 if st.button("Предсказать"):
     data = np.array([[pclass, sex, age, fare]])
     prediction = model.predict(data)
